4420afe5d77b0d793bfc002a1ef7aaec7fc62783
Vladislav Smirnov <lilcube205@gmail.com>
2024-10-22 18:18:01 +0300

    Lab1

---
+++ Container/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.container","l":"Container"},{"p":"org.container","l":"ContainerMain"}];updateSearchResults();
